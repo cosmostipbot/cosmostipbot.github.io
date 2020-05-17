007a81958ff324a84b7d889463d9dafdc7cec414
Tobias Schwarz <toschdev@gmail.com>
2020-05-17 16:41:21 +0200

    Built app

---
+++ precache-manifest.c6512a7d5a6c012476eb997e23e1a143.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8eee51a2b2133ff4d5688fe33be9503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beaae0ab6a79adff68ec",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "505ccbfe8e9ae33193a2",
+    "url": "/static/css/main.ed03878d.chunk.css"
+  },
+  {
+    "revision": "beaae0ab6a79adff68ec",
+    "url": "/static/js/2.737cf8a2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.737cf8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505ccbfe8e9ae33193a2",
+    "url": "/static/js/main.4e99a97c.chunk.js"
+  },
+  {
+    "revision": "8cddd9a39def77ad5fdc",
+    "url": "/static/js/runtime-main.e40f37c0.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
