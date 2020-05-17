c8efa3ad208f3cc7f66eed305cdc7e26fd68ec17
Tobias Schwarz <toschdev@gmail.com>
2020-05-17 16:26:21 +0200

    Website Layout

---
+++ src/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const phantom = {
+  display: 'block',
+  padding: '20px',
+  height: '60px',
+  width: '100%',
+};
+
+const foot = {
+    backgroundColor: "#F8F8F8",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "left",
+    padding: "20px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "60px",
+    width: "100%",
+};
+
+const link = {
+    color: "grey",
+}
+
+
+export default class Footer extends React.Component {
+	render() {
+		return(
+			<div style={phantom}>
+                <footer style={foot}>
+                    <span>&copy; All rights reserved. @cosmostipbot 2020</span>
+                    <span className="float-right">
+                        <a href="/terms-and-conditions" style={link}>
+                            <span className="ml-4">Terms & Conditions</span>
+                        </a>
+                        <a href="/privacy-policy" style={link}>
+                            <span className="ml-4">Privacy Policy</span>
+                        </a>
+                    </span>
+                </footer>
+			</div>
+		);
+
+	}
+}
