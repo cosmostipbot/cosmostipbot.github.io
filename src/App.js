baf9567f614020e701c438c837706dfbb0b1e18c
Tobias Schwarz <toschdev@gmail.com>
2020-05-17 17:00:35 +0200

    Deploy script for GitHub pages

--- src/App.js
+++ src/App.js
@@ -36,13 +36,12 @@ function App() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <Nav.Link href="#link">How-To</Nav.Link>
+          <NavDropdown title="About" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">FAQ</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Terms</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">About Us</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         {/* <Form inline>
