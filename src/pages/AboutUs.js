151a010022fcbe63aae7c3d5fb939aa5135fa70b
Tobias Schwarz <toschdev@gmail.com>
2020-06-04 21:36:40 +0200

    Add About Us content

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteRight } from "@fortawesome/pro-regular-svg-icons";
 
 export default class AboutUs extends Component {
   render() {
@@ -12,6 +14,60 @@ export default class AboutUs extends Component {
           <Row>
             <Col>
               <h1>About Us</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <i>@Cosmostipbot is a Cosmos Alliance collaborative effort.</i>
+              <br />
+              <br />
+              <div style={{ textAlign: "left" }}>
+                The first{" "}
+                <a href="https://cosmos.network" target="new">
+                  Cosmos
+                </a>{" "}
+                (ATOM) social tip application allows for seemless sending and
+                recieving of cryptocurrency to anyone <br />
+                <br />
+                via direct mentions and in public group chats. <br />
+                <br />
+                The application was co-created by Hubertus von Heyden (
+                <a href="https://twitter.com/Immasssi" target="new">
+                  @Immasssi
+                </a>
+                ) and Tobias Schwarz (
+                <a href="https://twitter.com/toschdev" target="new">
+                  @toschdev)
+                </a>
+                .<br />
+                <br />
+                <FontAwesomeIcon icon={faQuoteRight} size="lg" />{" "}
+                <i>
+                  Our mission is to provide an intuitive, convenient and fast
+                  way to reward content and creators on social media platforms.
+                </i>{" "}
+                <br />
+                <br />
+                <span style={{ marginLeft: "35px" }}>
+                  <i>
+                    We strive to improve app capabilities and features and
+                    expand to many social platforms, reaching both
+                  </i>
+                </span>{" "}
+                <br />
+                <br />
+                <span style={{ marginLeft: "35px" }}></span>
+                <i>
+                  the blockchain target audience as well as a broad userbase
+                  from outside the market
+                </i>
+                <br />
+                <br />
+                We always welcome community feedback and engagement so feel free
+                to reach out to us directly either on Telegram or Twitter.
+                <br />
+                <br />
+              </div>
             </Col>
           </Row>
         </Container>
